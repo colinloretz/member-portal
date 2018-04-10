b49bb5509d6a3b88f788f75193acc669f8950dfc
Colin Loretz <colin@mechani.sm>
2018-04-09 17:56:44 -0700

    Added default avatar for photo uploader to close #236

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,4 +15,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("default-avatar.jpeg")
+  end
 end
