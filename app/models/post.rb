38a265873c74cac178a25635f03200bbc7625bfc
Colin Loretz <colin@mechani.sm>
2018-04-11 12:18:55 -0700

    WIP Slack

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,25 +22,24 @@ class Post < ApplicationRecord
   end
 
   def send_to_slack
-    if ENV["SLACK_WEBHOOK_URL"]
+    if ENV['SLACK_WEBHOOK_URL']
       slack_attachment = {
-        fallback: "Required plain-text summary of the attachment.",
-        color: "#36a64f",
-        pretext: "A new conversation was started in the Member Portal",
-        author_name: "Author Name",
-        author_link: "author url",
-        author_icon: "author icon",
-        title: "Hello World",
-        title_link: "https://memberportal.renocollective.com/conversations/123",
-        text: "Excerpt of the post here",
-        image_url: "http://my-website.com/path/to/image.jpg",
-        thumb_url: "http://example.com/path/to/thumb.png",
-        footer: "Member Portal",
-        footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
-        ts: 123456789
+        fallback: 'A new conversation was started in the Member Portal',
+        color: '#36a64f',
+        pretext: 'A new conversation was started in the Member Portal',
+        author_name: member.fullname,
+        author_link: 'author url',
+        author_icon: 'author icon',
+        title: title,
+        title_link: 'https://memberportal.renocollective.com/conversations/123',
+        text: content.truncate(120, ' '),
+        image_url: image_url.to_s,
+        footer: 'Member Portal',
+        footer_icon: 'https://platform.slack-edge.com/img/default_application_icon.png',
+        ts: created_at.to_i
       }
 
-      notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"]
+      notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
       notifier.ping attachments: [slack_attachment]
     end
   end
